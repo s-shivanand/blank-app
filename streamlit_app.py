6b5dc512e19db5d76b657f9b1ce0b01004dc8d8a
Sharana Shivanand <107891112+s-shivanand@users.noreply.github.com>
2026-09-09 09:34:16 +0000

    guess

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("My new app")
 
-st.write("Yes!")
+# st.write("Sascha, you want to guess what my favourite cuisine is")
 
+st.button("Reset", type="primary")
+if st.button("Sascha, you want to guess what my favourite cuisine is?"):
+    st.write("Of course, it is Indian")
+
+if st.button("Next guess. Do you know how many languages I can read, write and speak?"):
+    st.write("4 - Kannada, English, Hindi, German")
