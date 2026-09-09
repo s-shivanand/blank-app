c60309b87657500ecb93ef9fd9c2f17020284e6f
Sharana Shivanand <107891112+s-shivanand@users.noreply.github.com>
2026-09-09 09:54:11 +0000

    guess as list

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,24 @@ st.title("My new app")
 
 # st.write("Sascha, you want to guess what my favourite cuisine is")
 
-st.button("Reset", type="primary")
-if st.button("Sascha, you want to guess what my favourite cuisine is?"):
-    st.write("Of course, it is Indian")
+#st.button("Reset", type="primary")
+#if st.button("Sascha, you want to guess what my favourite cuisine is?"):
+ #   st.write("Of course, it is Indian")
 
-if st.button("Next guess. Do you know how many languages I can read, write and speak?"):
-    st.write("4 - Kannada, English, Hindi, German")
+#if st.button("Next guess. Do you know how many languages I can read, write and speak?"):
+    #st.write("4 - Kannada, English, Hindi, German")
+
+
+with st.expander("You want to guess what my favourite cuisine is?"):
+    if st.button("Italian", type="tertiary"):
+        st.write("Nope.")
+    if st.button("Chinese", type="tertiary"):
+        st.write("Nope.")
+    if st.button("Japanese", type="tertiary"):
+        st.write("Nope.")
+    if st.button("Indian", type="tertiary"):
+        st.write("You are right!")
+        st.balloons()
+
+
+
