711ff39826e5c76f5c3b3e7b04d5ce4ab933019c
Sharana Shivanand <107891112+s-shivanand@users.noreply.github.com>
2026-09-09 09:09:01 +0000

    second deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
-st.write("Hello world!")
-st.write("Changes here reflect only in github.dev but not streamlit.dev")
+st.write("Yes!")
+
